7b15ab12b255b2809ac063f9d41b768186657661
jadashi97 <njadauniversal23@yahoo.com>
2024-04-03 17:58:05 -0500

    (feat): started database

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import InforBoxes from "@/components/InforBoxes";
 import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
+  console.log(process.env.MONGO_URI);
+
   return (
     <>
       <Hero />
