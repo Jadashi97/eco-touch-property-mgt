03beddea9a261f76bb35a48ae553f7f792cd18e5
jadashi97 <njadauniversal23@yahoo.com>
2024-05-22 19:11:17 -0500

    (feat):addded delete functionality. (fix):solve why wont show any properties

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -30,6 +30,7 @@ export const GET = async (request) => {
   }
 };
 
+// GET /api/properties
 export const POST = async (request) => {
   try {
     await connectDB();
@@ -119,3 +120,39 @@ export const POST = async (request) => {
     return new Response('Failed to add property', { status: 500 });
   }
 };
+
+// DELETE /api/properties/:id
+export const DELETE = async(request, {params}) => {
+  try {
+    const propertyId = params.id;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    await connectDB();
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) return new Response('Property Not Found', { status: 404 });
+
+    // Verify ownership
+    if (property.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    // Delete property
+    await property.deleteOne();
+
+    return new Response('Property Deleted', { status: 200 });
+
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+
+  }
+}
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -43,6 +43,36 @@ const ProfilePage = () => {
     }
   }, [session]);
 
+  const handleDeleteProperty = async (propertyId) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this property?"
+    );
+
+    if (!confirmed) {
+      return;
+    }
+
+    try {
+      const res = await fetch(`/api/properties/${propertyId}`, {
+        method: "DELETE",
+      });
+
+      if (res.status === 200) {
+        // Remove property from state
+        const updateProperties = properties.filter(
+          (property) => property._id !== propertyId
+        );
+        setProperties(updateProperties);
+
+        console.log("Property deleted");
+      } else {
+        console.error("Failed to delete property");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section className="bg-blue-50">
       <div className="container m-auto py-24">
@@ -108,6 +138,7 @@ const ProfilePage = () => {
                       <button
                         className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
                         type="button"
+                        onClick={() => handleDeleteProperty(property._id)}
                       >
                         Delete
                       </button>
