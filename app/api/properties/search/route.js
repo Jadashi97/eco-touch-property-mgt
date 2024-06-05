a64a48fdca8ba822bd442bf30f236bc569f4610e
jadashi97 <njadauniversal23@yahoo.com>
2024-06-05 12:23:23 -0500

    (feat):added fetch code to fetch the properties. (fix): solve the propertycard not allowing using sort

--- app/api/properties/search/route.js
+++ app/api/properties/search/route.js
@@ -9,7 +9,38 @@ try {
     const {searchParams} = new URL(request.url);
     const location = searchParams.get('location');
     const propertyType = searchParams.get('propertyType');
+
+
+    // Create a regex pattern for location search
+    const locationPattern  = new RegExp(location, 'i');
+
+    // Create a query to search for properties
+    let query = {
+        $or: [
+            {name: locationPattern},
+            {description: locationPattern},
+            {'location.street': locationPattern},
+            {'location.city': locationPattern},
+            {'location.state': locationPattern},
+            {'location.zipcode': locationPattern},
+        ],
+    };
+
+    // Only check for property type if it's not 'All'
+    if(propertyType && propertyType !== 'All'){
+        const typePattern = new RegExp(propertyType, 'i');
+        query.type  = typePattern;
+    };
+
+    const properties = await Property.find(query);
+
+
+    return new Response(JSON.stringify(properties), {
+        status: 200,
+    });
+
 } catch (error) {
-    
+    console.log(error);
+     return new Response('something went wrong!',{status: 500})
 }
 }
---
+++ app/properties/search-results/page.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+const SearchResultsPage = () => {
+  const searchParams = useSearchParams();
+
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const location = searchParams.get("location");
+  const propertyType = searchParams.get("propertyType");
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
+
+        if (res.status === 200) {
+          const data = await res.json();
+          console.log(data);
+          setProperties(data);
+        } else {
+          setProperties([]);
+        }
+      } catch (error) {
+        console.error("Error fetching properties:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProperties();
+  }, [location, propertyType]);
+
+  return <div>Search results</div>;
+};
+
+export default SearchResultsPage;
